aadcc09d8666e8334fbd46e4f71063de6f907aec
mattrudin <matthias.rudin@live.com>
2018-08-03 07:12:11 +0200

    Finished SearchBar.js; App.js will be next

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,16 +4,21 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			term: ''
+		}
 		this.search = this.search.bind(this);
 		this.handleTermChange = this.handleTermChange.bind(this);
 	}
 
 	search() {
-		//Step 69
+		this.props.onSearch(this.state.term);
 	}
 
 	handleTermChange(event) {
-		this.props.onSearch(event.target.value);
+		this.setState({
+			term: event.target.value
+		});
 	}
 
 	render() {
@@ -21,7 +26,7 @@ class SearchBar extends React.Component {
 			<div className="SearchBar">
          		<input placeholder="Enter A Song, Album or Artist" 
          			   onChange={this.handleTermChange} />
-          		<a>SEARCH</a>
+          		<a onClick={this.search} >SEARCH</a>
         	</div>
 			);
 	}
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './TrackList.css';
 import Track from '../Track/Track';
 
+//TrackList.js will be used by SearchResult.js and Playlist.js; the passed props has to be the same name in both components
+
 class TrackList extends React.Component{
 	render() {
 		return(
